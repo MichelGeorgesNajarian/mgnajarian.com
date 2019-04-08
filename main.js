0f5df80d66e5c470fb794fc952ffa3cb87869bf3
MichelGeorgesNajarian <migeo.najarian@gmail.com>
2019-04-08 19:39:23 -0400

    Website first step, Header with options and loading screen

---
+++ main.js
@@ -0,0 +1,7 @@
+/*eslint-env browser*/
+window.addEventListener("load", function() {
+    alert("bananas");
+    document.getElementById("ready").style.display = "block";
+    document.getElementById("loading").style.display = "none";
+});
+
