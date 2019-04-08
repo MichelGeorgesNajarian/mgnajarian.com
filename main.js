47502ea584c496f91950f50bb2020e2dde57c81c
MichelGeorgesNajarian <migeo.najarian@gmail.com>
2019-04-08 19:56:54 -0400

    Website first step, Header with options and loading screen, removed alert that I had forgotten to delete in previous commit

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 /*eslint-env browser*/
 window.addEventListener("load", function() {
-    alert("bananas");
     document.getElementById("ready").style.display = "block";
     document.getElementById("loading").style.display = "none";
 });
