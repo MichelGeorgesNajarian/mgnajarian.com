249b22362298838a7c50f4e388c6d7d89fe9c616
MichelGeorgesNajarian <michel.georges@mgnajarian.com>
2019-05-15 07:23:17 -0400

    menu done

--- main.js
+++ main.js
@@ -4,3 +4,37 @@ window.addEventListener("load", function() {
     document.getElementById("loading").style.display = "none";
 });
 
+//document.getElementById("nav").addEventListener('mouseenter',showHide);
+//document.getElementById("nav").addEventListener('mouseout',showHide);
+//
+//function showHide() {
+//    var allDivs = document.getElementsByClassName("items");
+//    for (var i = 0; i < allDivs.length; i++) {
+//        var isShown = allDivs[i].classList[1];
+//        switch (isShown) {
+//            case "hidden":
+//                allDivs[i].classList.remove("hidden");
+//                allDivs[i].classList.add("shown");
+//                break;
+//            case "shown":
+//                allDivs[i].classList.remove("shown");
+//                allDivs[i].classList.add("hidden");
+//                break;
+//        }
+//    }
+//}
+
+var menuAttributes = document.querySelectorAll("div#nav div.items a.menu-option");
+for (var i = 0; i < menuAttributes.length; i++) {
+    menuAttributes[i].addEventListener('click', clickedMenu);
+}
+
+function clickedMenu(evt) {
+    var allAttr = document.querySelectorAll("div#nav div.items a.menu-option");
+    for (var i = 0; i < allAttr.length; i++) {
+        allAttr[i].classList.remove("clicked");
+        allAttr[i].classList.add("notClicked");
+    }
+    evt.target.classList.remove("notClicked");
+    evt.target.classList.add("clicked");
+}
