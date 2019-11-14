57991dcac0c05816c62a65a7053ef52eb8d4a39b
MichelGeorgesNajarian <migeo.najarian@gmail.com>
2019-11-14 17:21:14 -0500

    starting mobile support and animations

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-/*eslint-env browser*/
-window.addEventListener("load", function() {
+document.getElementById("temp").addEventListener("click", function () {
+    var nav = document.getElementById("nav");
+    if (nav.style.display == "block") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+});
+
+
+window.addEventListener("load", function () {
     document.getElementById("ready").style.display = "block";
     document.getElementById("loading").style.display = "none";
 });
@@ -24,13 +33,22 @@ window.addEventListener("load", function() {
 //    }
 //}
 
-var menuAttributes = document.querySelectorAll("div#nav div.items a.menu-option");
+function displayNav() {
+    var nav = document.getElementById("nav");
+    nav.style.display = "display: -webkit-box";
+    nav.style.display = "-moz - box";
+    nav.style.display = "-ms - flexbox";
+    nav.style.display = "-webkit - flex";
+    nav.style.display = "flex";
+}
+
+var menuAttributes = document.querySelectorAll("ul#nav li a.menu-option");
 for (var i = 0; i < menuAttributes.length; i++) {
     menuAttributes[i].addEventListener('click', clickedMenu);
 }
 
 function clickedMenu(evt) {
-    var allAttr = document.querySelectorAll("div#nav div.items a.menu-option");
+    var allAttr = document.querySelectorAll("ul#nav li a.menu-option");
     for (var i = 0; i < allAttr.length; i++) {
         allAttr[i].classList.remove("clicked");
         allAttr[i].classList.add("notClicked");
