6ea89f0692ba8d4a07f298241cceb9af1b1db11a
MichelGeorgesNajarian <michel.georges@mgnajarian.com>
2024-11-11 21:25:31 -0500

    feat(linting): added support for eslint with tailwind and prettier configs

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintPluginTailwindCSS from "eslint-plugin-tailwindcss";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: [
+      "dist/",
+      "**/node_modules/*",
+      "coverage",
+      "**/.next/*",
+      "*.lock",
+      "**/out/*",
+      "next-env.d.ts",
+      "package.json",
+      "package-lock.json",
+    ],
+  },
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    ignores: ["dist/**/*"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...eslintPluginTailwindCSS.configs["flat/recommended"],
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: false,
+          endOfLine: "auto",
+          trailingComma: "all",
+          bracketSpacing: true,
+          quoteProps: "consistent",
+        },
+      ],
+    },
+  },
+];
