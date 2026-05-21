aa532c48bcbae4db3aa401a3dd4e2a2cb2120a29
MichelGeorgesNajarian <michel.georges@mgnajarian.com>
2026-05-21 00:54:03 -0400

    feat: initial nextjs setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,6 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import eslintPluginTailwindCSS from "eslint-plugin-tailwindcss";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -33,7 +32,6 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  ...eslintPluginTailwindCSS.configs["flat/recommended"],
   eslintPluginPrettierRecommended,
   {
     rules: {
